18b771b38a77bee23f2b9eecf048d9da65556672
Mara Rosa <edimara.rosa@outlook.com>
2021-01-02 23:36:32 -0300

    create generic class for holding status response and loading

--- app/src/main/java/com/projects/tmdb/ui/listing/ListingActivity.kt
+++ app/src/main/java/com/projects/tmdb/ui/listing/ListingActivity.kt
@@ -3,9 +3,14 @@ package com.projects.tmdb.ui.listing
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.projects.tmdb.R
 import com.projects.tmdb.model.Movie
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.listing_activity.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 /**
  * Shows list of movie/show
@@ -13,8 +18,34 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ListingActivity : AppCompatActivity() {
 
+    private val list = ArrayList<Movie>()
+    private val viewModel by viewModels<ListingViewModel>()
+    private lateinit var moviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.listing_activity)
+
+        init()
+        subscribeUi()
     }
+
+    private fun init() {
+        val layoutManager = LinearLayoutManager(this)
+        rvMovies.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            rvMovies.context,
+            layoutManager.orientation
+        )
+
+        rvMovies.addItemDecoration(dividerItemDecoration)
+        moviesAdapter = MoviesAdapter(this, list)
+        rvMovies.adapter = moviesAdapter
+    }
+
+    private fun subscribeUi() {
+
+    }
+
 }
